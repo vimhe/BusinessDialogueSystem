14c6268a8a8915015023acbce674242630242f9a
Henry <118166128+vimhe@users.noreply.github.com>
2024-05-14 23:52:09 +0800

    Delete Source directory

--- Source/Dialogue/Dialogue.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Dialogue.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dialogue, "Dialogue" );
- 
